f858e5ad96622d229f359ef2e64e4272b9d32f09
tanyongkeat <tyk9079@gmail.com>
2022-03-21 16:46:56 +0800

    made config to read from env

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
 
-user = 'user'
-pw = 'pw'
+user = os.environ.get('PSQL_USER')
+pw = os.environ.get('PSQL_PW')
 host = 'psqldb'
-port = '1234'
+port = os.environ.get('PSQL_PORT')
 url = f'{host}:{port}'
-database = 'database'
+database = os.environ.get('PSQL_DATABASE')
 
 DB_URL = f'postgresql+psycopg2://{user}:{pw}@{url}/{database}'
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'yeah-babe'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hello-hello-hello'
     SQLALCHEMY_DATABASE_URI = DB_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
